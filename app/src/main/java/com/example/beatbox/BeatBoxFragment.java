56c150253a4c1ed9b06c13028bf295fe61344ba3
shashank-vats <shashank.vats8@gmail.com>
2020-04-20 11:55:23 +0530

    Added unit tests

--- app/src/main/java/com/example/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/example/beatbox/BeatBoxFragment.java
@@ -29,6 +29,7 @@ public class BeatBoxFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
 
         mBeatBox = new BeatBox(Objects.requireNonNull(getActivity()));
     }
@@ -40,6 +41,12 @@ public class BeatBoxFragment extends Fragment {
         binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
         return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBeatBox.release();
     }
 
     private class SoundHolder extends RecyclerView.ViewHolder {
